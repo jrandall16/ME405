c193ca258a6c220559df32fc9df2831c391b1d87
cobush <connor.bush97@gmail.com>
2020-02-05 21:18:52 -0800

    Controller changes

--- lab_2/controller.py
+++ lab_2/controller.py
@@ -5,9 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-#import pyb
-#import encoder
-#import motor_driver
+import utime
 
 class Controller:
     ''' This class controlls the motor using proportional only control
@@ -15,30 +13,55 @@ class Controller:
     
     def __init__ (self, position):
         ''' This method initializes the controller.
-        @ param Kp:
-        @ param setpoint:
-        @ param gain:
+        @ param Kp: Kp is the proportional gain for the controller
+        @ param setpoint: setpoint is the desired value that eh motor is trying to reach
+        @ param gain: gain is the motors steady state gain determined experimentally from step response data
         '''
-        self.Kp = 0.25
-        self.setpoint = 4
+        
+        ## Kp is the proportional gain for the controller
+        self.Kp = 0.001
+
+        ## setpoint is the desired value for the motor, in this case it is the desired encoder position
+        self.setpoint = 16000
+
+        ## gain is the motor steady state gain
         self.gain = 10
+
+        ## position is the current value for the motor, in this case it is the current encoder position
         self.position = position
         
     def controlla (self):
         ''' This method runs the control algorithm and returns an error reading 
         to be used in the control system.
         '''
-        
+
+        ## errror is the deviation from the desired value
         error = self.setpoint - self.position
+
+        ## actuate is the signal appleid to the motor based on the error. The error is multiplied by Kp and used as the PWM signal sent to the motor
         actuation = self.Kp*error
+
         return actuation
         
     def setPoint (self):
         ''' This method assigns the setpoint a new value for changes in set point
         during operation.
         '''
+        
+        ## setpoint is reset to the value of point while running the motor controller
+        self.setpoint = point
+
+
     def setGain (self):
         ''' This method assigns the control gain(s) a new value for special cases
         during operation. i.e. during motor startup to overcome stiction.
         '''      
-        
+        ## Kp is reset to the value of gain while running the motor controller
+        self.Kp = gain
+
+    def readData(self, position):
+        ''' This method reads the data from the serial port and organizes it into columns of time and motor position.
+        '''
+
+        self.data = (utime.ticks_ms(), ',', position, '\n')
+
