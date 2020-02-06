4583946b3197c680f96777d2c3cd867e4e166928
cobush <connor.bush97@gmail.com>
2020-02-05 21:44:39 -0800

    Controller Init Update

--- lab_2/controller.py
+++ lab_2/controller.py
@@ -11,33 +11,34 @@ class Controller:
     ''' This class controlls the motor using proportional only control
     '''
     
-    def __init__ (self):
+    def __init__ (self, Kp, setpoint, gain):
         ''' This method initializes the controller.
         @ param Kp: Kp is the proportional gain for the controller
         @ param setpoint: setpoint is the desired value that eh motor is trying to reach
         @ param gain: gain is the motors steady state gain determined experimentally from step response data
         '''
         
-        ## Kp is the proportional gain for the controller
-        self.Kp = 0.005
+        ## Kp is the proportional gain for the controller and is defined when class Controller is called
+        self.Kp = Kp
 
-        ## setpoint is the desired value for the motor, in this case it is the desired encoder position
-        self.setpoint = 16000
+        ## setpoint is the desired value for the motor, in this case it is the desired encoder position. It is defined when class Controller is called
+        self.setpoint = setpoint
 
-        ## gain is the motor steady state gain
-        self.gain = 10
+        ## gain is the motor steady state gain and is defined when class Controller is called
+        self.gain = gain
         
     def outputValue (self, position):
         ''' This method runs the control algorithm and returns an error reading 
         to be used in the control system.
         '''
 
-        ## errror is the deviation from the desired value
+        ## error is the deviation from the desired setpoint value
         error = self.setpoint - position
 
-        ## actuate is the signal appleid to the motor based on the error. The error is multiplied by Kp and used as the PWM signal sent to the motor
+        ## actuation is the signal appleid to the motor based on the error. The error is multiplied by Kp and used as the PWM signal sent to the motor
         actuation = self.Kp*error
 
+        # return actuation so outputValue() can be used to control the motor
         return actuation
         
     def setPoint (self, point):
--- lab_2/main.py
+++ lab_2/main.py
@@ -10,20 +10,32 @@ import encoder as enc
 import motor_driver
 import utime
 import controller
+
+# call class MotorDriver()
 motor = motor_driver.MotorDriver()
-# motor.set_duty_cycle(30)
+
+# call class Controller()
+ctr = controller.Controller()
+
+# define the encoder that is used to read the motor position
 encB = enc.Encoder('B')
 
-ctr = controller.Controller()
-# output_level = ctr.setOutput(encB.read())
-# motor.set_duty_cycle(output_level)
+# initiate a try block with exception for emergency stop by keyboard interupt
 try:
     print ('firstline')
     while True:
+        #set the duty cycle to the output of controller function outputValue() using the current position of the encoder to determine the actuation signal
         motor.set_duty_cycle(ctr.outputValue(encB.read()))
+
+        # print the data into the terminal to be read by the pc
         print (str(encB.read()) + ',' + str(utime.ticks_ms()))
+
+        # print data every 10 milliseconds
         utime.sleep_ms(10)
 
+
+
+# set duty cycle of the motor to 0 if a keyboard interput is thrown
 except KeyboardInterrupt:
     motor.set_duty_cycle(0)
     
