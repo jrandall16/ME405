bfac3144b5c271e6f649647ecfa5cd012bac274f
cobush <connor.bush97@gmail.com>
2020-02-19 13:56:26 -0800

    added code to filter the data

--- lab_4/main.py
+++ lab_4/main.py
@@ -15,7 +15,7 @@ s0 = task_share.Share ('I', thread_protect = False, name = "Share_0")
 # define a queue to be used as a buffer for interrupt timestamp data
 q0 = task_share.Queue ('I', 68, thread_protect = False, overwrite = False, name = "Queue_0")
 
-## using the assigned timer, collect the timestamp data from the IR signal.
+## using a defined timer, collect the timestamp data from the IR signal.
 def callback_fun (timer):
     # put the timestamp data into the queue when the share value is 0
     while s0.get() == 0:
@@ -28,13 +28,14 @@ def callback_fun (timer):
 # assign pinA8 as an input pin to read the incoming data from the IR sensor
 pinA8 = pyb.Pin (pyb.Pin.board.PA8, pyb.Pin.IN)
 
-# set up the correct time for pinA8. Assign the timer a 16-bit period and a prescaler of 79 to collect accurate timestamps
-tim = pyb.Timer (1, prescaler=79, period=0xFFFF)
+# set up the correct timer for pinA8. Assign the timer a 16-bit period and a prescaler of 79 to collect accurate timestamps
+tim = pyb.Timer (1, prescaler=79, period=0xFFFF, callback = callback_fun)
 
-# set up the timer object to detect edges using interrupt detection
+# set up the timer object to detect edges 
 ch1 = tim.channel(1, pyb.Timer.IC, polarity = pyb.Timer.BOTH, pin = pinA8)
 
-callback = ch1.callback(callback_fun)
+
+#tim.callback(callback_fun)
 
 if __name__ == "__main__":
 
@@ -47,13 +48,32 @@ if __name__ == "__main__":
             s0.put(0)    
 
             #find the difference between edges 
-            # newdata = []
+            subdata = []
+            for i in data:
+                if data.index(i) > 1 and data.index(i) < 67:
+                # if the index is a factor of two, find the difference of the two values and put it in a list
+                    if data.index(i)%2 != 0:
+                      subdata.append(int(data[data.index(i)] - data[data.index(i)-1]))
 
-            # for i in data:
-            #     if data.index(i) > 1 and data.index(i) < 67:
-            #     # if the index is a factor of two, find the difference of the two values and put it in a list
-            #         if data.index(i)%2 != 0:
-            #           newdata.append(data[data.index(i)] - data[data.index(i)-1]) 
+            # equate the differences to a 4 byte binary number    
+            isrdata = []
+            # find a value that splits all values in newdata. The low values represent 0's and the high values represent 1's
+            midvalue = 0
+            for i in subdata:
+                if i < midvalue:
+                    isrdata.append(0)
+                if i > midvalue:
+                    isrdata.append(1)
+
+            # the data from isr data is written in reversed binary, reverse the order of isrdata to generate accurate binary data
+            bindata = []
+
+            # shift each value read from isrdata to the left to generate the appropriate binary data
+            # << shifts the value to the left by the value of the index
+            # charlie mentioned this was one way shift the data around, not sure if it's the best way. its raising the value to the power of the index instead of shifting it
+            for i in range(len(isrdata)):
+                bindata.append(isrdata[i] << i)
+
         pass
 
 
